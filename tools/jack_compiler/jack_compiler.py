d49a3c36a8a15c957f2d869cd61924c4a0751ef9
Michael Løiten <michael.l.magnussen@gmail.com>
2022-08-31 15:26:49 +0200

    Swap tools with dummy files

---
+++ tools/jack_compiler/jack_compiler.py
@@ -0,0 +1,7 @@
+"""Dummy file for the jack_compiler package."""
+
+if __name__ == "__main__":
+    print(
+        f"{__file__} not found. "
+        "Please add or link the Jack compiler developed in the Nand2Tetris course"
+    )
---
+++ tools/vm_translator/vm_translator.py
@@ -0,0 +1,7 @@
+"""Dummy file for the vm_translator package."""
+
+if __name__ == "__main__":
+    print(
+        f"{__file__} not found. "
+        "Please add or link the VM translator developed in the Nand2Tetris course"
+    )
